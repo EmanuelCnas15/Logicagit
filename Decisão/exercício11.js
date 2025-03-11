58a705e7e708caca5b5fa067d1ca397edb297c7f
EmanuelCnas15 <nascimentojoaoemanuel@gmail.com>
2025-03-11 15:19:46 -0300

    Primeiro Commit

--- Decisão/exercício11.js
+++ Decisão/exercício11.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')();
 let senha = prompt('Digite a senha: ');
 
 // Senha correta
-const senhaCorreta = '12345678';
+let senhaCorreta = '12345678';
 
 if (senha === senhaCorreta){
     console.log('Acesso permitido');
