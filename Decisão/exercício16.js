b816b0ce905e5c6a82acf80c581755ba76e7d960
EmanuelCnas15 <nascimentojoaoemanuel@gmail.com>
2025-03-17 12:54:22 -0300

    primeiro commit

--- Decisão/exercício16.js
+++ Decisão/exercício16.js
@@ -1,11 +1,11 @@
 const prompt = require('prompt-sync')();
 
-const valorCompra = parseFloat(prompt('Digite o valor da sua compra: R$ '));
+let valorCompra = prompt('Digite o valor da sua compra: R$ ');
 let valorFinal;
 
 if (valorCompra < 100) {
     valorFinal = valorCompra;
-} else if (valorCompra >= 100 && valorCompra < 500) {
+} else if (valorCompra < 500) {
     valorFinal = valorCompra * 0.90; // 10% de desconto
 } else {
     valorFinal = valorCompra * 0.80; // 20% de desconto
