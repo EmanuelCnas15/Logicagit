c5041f209ae1f043b8cd30044b8cac4c93ff818a
EmanuelCnas15 <nascimentojoaoemanuel@gmail.com>
2025-03-31 14:15:53 -0300

    primeiro commit

---
+++ Repetição/exercício14.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+let numero = 10;
+
+while (numero >= 1) {
+    console.log(numero);
+    numero--;
+}
---
+++ Repetição/exercício15.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+let numero = 1;
+let soma = 0;
+while (numero <= 100) {
+    soma += numero;
+    numero++;
+}
+console.log(`A soma dos números de 1 a 100 é: ${soma}`);
