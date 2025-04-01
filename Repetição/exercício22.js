b6f8f236405812efda780dbf2f58998e3f19e0d7
EmanuelCnas15 <nascimentojoaoemanuel@gmail.com>
2025-04-01 14:13:30 -0300

    primeiro commit

--- Repetição/exercício22.js
+++ Repetição/exercício22.js
@@ -1,1 +1,12 @@
 const prompt = require('prompt-sync')();
+let soma = 0, notas, media;
+let quantidade = 0;
+while (notas != -1){
+    notas = Number(prompt('Digite a nota do aluno: '));
+    if ( notas != -1 ){
+        soma += notas;
+        quantidade++;
+    }
+}
+media = soma / quantidade;
+console.log('A média das notas é: ' + media);
