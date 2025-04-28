0258cbfef4d1b3296814583e87e7542478821db1
EmanuelCnas15 <nascimentojoaoemanuel@gmail.com>
2025-04-28 08:46:10 -0300

    primeiro commit

---
+++ Array/exemplo_repetição.js
@@ -0,0 +1,6 @@
+const prompt = require ('prompt-sync')();
+let numeros = [];
+for (let i = 0; i < 9; i++){
+    numeros[i] = Number(prompt("Digite um número para a posção " +1));
+}
+console.log("Vetor completo:" , numeros);
