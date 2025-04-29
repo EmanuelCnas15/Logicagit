a7e3a48bcf6d67faa9b0cdd5567a6d87cfee0178
EmanuelCnas15 <nascimentojoaoemanuel@gmail.com>
2025-04-29 13:35:32 -0300

    primeiro commit

--- Array/exercício27.js
+++ Array/exercício27.js
@@ -5,6 +5,7 @@ for (let i = 0; i < 10; i++) {
     salario[i] = prompt("Digite o salário do funcionário " + i + ": ");
 }
 console.log("Os salários dos funcionários são: ");
+
 for (let i = 0; i < salario.length; i++) {
     console.log(salario[i]);
 }
